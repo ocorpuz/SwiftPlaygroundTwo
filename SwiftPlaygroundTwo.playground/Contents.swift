4c7c5388cc143ad55af6f88b3017c4165af0b470
ocorpuz <59846122+ocorpuz@users.noreply.github.com>
2020-02-04 10:11:20 -1000

    Completed 1-4

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
 var str = "Hello, playground"
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
+else
+{
+    print("I need to work harder in this class")
+}
+
+var gradeA = true
+if gradeA
+{
+    print("I really like this class")
+}
+else
+{
+    print("I'm not so sure about coding")
+}
